2be30660f1956cee97d400a66d2bb8febbdd37bc
Maxi-Coronel <maxicoronel1992@gmail.com>
2021-07-24 17:26:24 -0300

    17-26

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -16589,8 +16589,7 @@ let comparativo =[];
 
 let poke = 0;
 
-let click = document.getElementById("click");
-click.addEventListener('click',VerPokemon);
+$(`#click`).click(VerPokemon)
 
 const boton = document.getElementById("button");
 boton.addEventListener('click', anyadirPoke);
@@ -16598,8 +16597,8 @@ boton.addEventListener('click', anyadirPoke);
 const buscado = 0;
 
 function VerPokemon(){
-    poke = document.getElementById("buscador").value;
-    let buscado = pokedex.filter(pokedex => pokedex.name.english == poke);
+    poke = $(`#buscador`).val();
+    let buscado = pokedex.filter(e => e.id == poke);
 
     boton.setAttribute('marcador', buscado[0].id);
 
@@ -16610,7 +16609,7 @@ function VerPokemon(){
       insertar1b.innerHTML = `<p>${buscado[0].name.english}</p>`;
 
     let insertar1c = document.getElementById("img");
-      insertar1c.setAttribute('src', `./pokemon.json-master/images/${buscado[0].id}.png`);
+      insertar1c.setAttribute('src', `./pokemon.json-master/images/00${buscado[0].id}.png`);
       document.getElementById('img').style.width = '300px';
 
     let insertar2a = buscado.map(function(bar){
@@ -16625,11 +16624,50 @@ function VerPokemon(){
 };
 
 function anyadirPoke(evento) {
-  const nuevo = pokedex.filter(pokedex => pokedex.id == evento.target.getAttribute('marcador'));
-  comparativo.push(nuevo[0]);
-
-  let comparador = comparativo.map(function(bar){
-    return '<ul>'+'<li>'+bar.name.english+'</li>'+'<li>'+'Tipo: '+bar.type+'</li>'+'<li>'+'HP: '+bar.base.HP+'</li>'+'<li>'+'Attack: '+bar.base.Attack+'</li>'+'<li>'+'Defense: '+bar.base.Defense+'</li>'+'<li>'+'Sp. Attack: '+bar.base.SpAttack+'</li>'+'<li>'+'Sp. Defense: '+bar.base.SpDefense+'</li>'+'<li>'+'Speed: '+bar.base.Speed+'</li>'+'</ul>'
-  });
-  document.getElementById("comparador").innerHTML = comparador;
+  //llamamos al storage
+  let compVacio = JSON.parse(localStorage.getItem(`comparativo`));
+  //condicional de storage
+  if (localStorage.getItem(`comparativo`) != null) {
+    const nuevo = pokedex.filter(e => e.id == evento.target.getAttribute('marcador'));
+    compVacio.push(nuevo[0]);
+    localStorage.setItem(`comparativo`, JSON.stringify(compVacio));
+  }else {
+    localStorage.clear();
+    const nuevo = pokedex.filter(e => e.id == evento.target.getAttribute('marcador'));
+    comparativo.push(nuevo[0]);
+    localStorage.setItem(`comparativo`, JSON.stringify(comparativo));
+  }
 };
+
+function imprimirPoke() {
+  let imprimir = JSON.parse(localStorage.getItem(`comparativo`));
+  console.log(imprimir);
+  if (imprimir != null) {
+    imprimir.forEach(e => {
+        $(`#comparador`).append(`<div class="margin">
+                                  <input type="button" class="btn btn-danger esqDerecha" value="x" marcador="${e.id}">
+                                  <ul>
+                                    <li>ID = ${e.id}</li>
+                                    <li>Nombre = ${e.name.english}</li>
+                                    <li>Tipo = ${e.type}</li>
+                                    <li>HP = ${e.base.HP}</li>
+                                    <li>Ataque = ${e.base.Attack}</li>
+                                    <li>Defensa = ${e.base.Defense}</li>
+                                    <li>Super ataque = ${e.base.SpAttack}</li>
+                                    <li>Super defensa = ${e.base.SpDefense}</li>
+                                    <li>Velocidad = ${e.base.Speed}</li>
+                                  </ul>
+                                  </div>`)
+    });
+  }
+}
+
+function borrarPoke(evento) {
+  let borrar = JSON.parse(localStorage.getItem(`comparativo`));
+  let actualizado = borrar.filter(e => e.id != evento.target.getAttribute('marcador'));
+  localStorage.setItem(`comparativo`, JSON.stringify(actualizado));
+  location.reload();
+}
+imprimirPoke();
+$(`#button`).click(imprimirPoke);
+$(`#borrar`).click(borrarPoke);
